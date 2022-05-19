1b4629870757a3592d3583229ab7bd72a1c3d29a
rguu0007 <rguu0007@student.monash.edu>
2022-05-19 14:50:23 +0800

    feat: change variable for testing the navibar

--- techstackz/src/App.js
+++ techstackz/src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import TechInfo from './TechInfo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          This is the index page of FIT3170 made by frontend group
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (
+  <div className="App">
+      <Navbar />
     </div>
-  );
+  );
 }
 
 export default App;
