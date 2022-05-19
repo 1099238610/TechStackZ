04003c2683a7862964ac51e7ca49a985622845dd
Saketh <sjon0027@student.monash.edu>
2022-05-19 15:18:17 +0800

    update navbar location on app

--- techstackz/src/App.js
+++ techstackz/src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.css';
 import TechInput from './TechInput';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar />
     <div className="App">
       <TechInput/>
     </div>
+    </>
   );
 }
 
