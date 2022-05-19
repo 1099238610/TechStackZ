06f137a27a523c019c57e855011707866919a603
jdon0030 <jdon0030@student.monash.edu>
2022-05-19 15:11:36 +0800

    Feat: add a simple footer (no css)

--- techstackz/src/App.js
+++ techstackz/src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.css';
 import TechInput from './TechInput';
+import Footer from './footer';
 
 function App() {
   return (
     <div className="App">
       <TechInput/>
+      <Footer/>
     </div>
   );
 }
