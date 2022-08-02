780b5972291d547057e4f15b026819fec6b99fed
zsuu0015 <zsuu0015@student.monash.edu>
2022-08-03 00:06:33 +1000

    Feat: Add the navbar in order to let the user navigate their interested pages

---
+++ techstackz/src/components/NewNavbar/Narbar.js
@@ -0,0 +1,81 @@
+import React, {useEffect, useState} from 'react'
+import {Link} from "react-router-dom";
+import {Button} from "../Button/Button";
+import './Narbar.css';
+
+function Navbar() {
+  // states
+  const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
+
+  // set menu status by detecting if it was clicked or not
+  const handleClick = () => setClick(!click);
+
+  // set the menu close in mobile status
+  const closeMobileMenu = () => setClick(false)
+
+  // if the window inner width is less than 960, show the menu button
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  // show the button
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className={"navbar"}>
+        {/* TechStackZ logo */}
+        <div className={"navbar-container"}>
+          <Link to={"/"} className={"navbar-logo"} onClick={closeMobileMenu}>
+            TechStackZ
+          </Link>
+          {/* if show the navbar menu with the condition of window width */}
+          <div className={"menu-icon"} onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+          </div>
+          {/* navbar items*/}
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className={'nav-item'}>
+              <Link to={'/'} className={'nav-links'} onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className={'nav-item'}>
+              <Link to={'/techSearch'} className={'nav-links'} onClick={closeMobileMenu}>
+                Search
+              </Link>
+            </li>
+            <li className={'nav-item'}>
+              <Link to={'/techComparison'} className={'nav-links'} onClick={closeMobileMenu}>
+                Comparison
+              </Link>
+            </li>
+            <li className={'nav-item'}>
+              <Link to={'/techRecommendation'} className={'nav-links'} onClick={closeMobileMenu}>
+                Recommendation
+              </Link>
+            </li>
+            <li>
+              <Link to={'/sign-up'} className={'nav-links-mobile'} onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {/* decide whether show the sign-up button depending on the window width*/}
+          {button && <Button buttonStyle={'btn-outline'}>SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
