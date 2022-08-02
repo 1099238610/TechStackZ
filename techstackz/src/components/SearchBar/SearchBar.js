802ae67bc83e9e3943cb89f365ae3151a9010ef2
zsuu0015 <zsuu0015@student.monash.edu>
2022-08-03 00:07:28 +1000

    Feat: Add the search bar to let the user be able to search and pass the input to the TechInformation page

---
+++ techstackz/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import "./SearchBar.css"
+import {Button} from "../Button/Button";
+
+function SearchBar() {
+  // states
+  const [userInput, setUserInput] = useState("Python")
+
+  // collect user input and store it to userInput state
+  const handleTextChange = e => {
+    setUserInput(e.target.value)
+  }
+
+  return (
+    <>
+      {/* search bar */}
+      <div className={"search"}>
+        <input className={"search__input"} defaultValue={"Python"} type={"text"} id={"search"}
+               onChange={handleTextChange}/>
+      </div>
+      <p></p>
+      <p></p>
+      {/* search button and link to the techInformation page with userInput */}
+      <Link to={`/techInformation/${userInput}`}>
+        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GO SEARCH</Button>
+      </Link>
+    </>
+  )
+}
+
+export default SearchBar
