f90edc3ed24de114dcbe23e9d8d3218ce6663afd
ypen0032 <ypen0032@student.monash.edu>
2022-08-04 20:34:56 +1000

    Feat: add tag comparison page template

--- techstackz/src/pages/TechComparison.js
+++ techstackz/src/pages/TechComparison.js
@@ -1,6 +1,14 @@
 import React from "react";
 import '../App.css'
-
+import CompareInput from "../components/CompInput/CompInput"
 export default function TechComparison() {
-  return <h1 className={"techComparison"}>Tech Comparison</h1>
+  return (
+    <>
+    <h1 style={{marginTop:"2em"}}>Explore The Difference</h1>
+    <CompareInput/>
+    </>
+    
+    
+  
+  )
 }
