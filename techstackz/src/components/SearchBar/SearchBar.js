ef2baef0d11aeacf6771d5c6ab37e6771a42c7ed
sjon0027 <sjon0027@student.monash.edu>
2022-09-01 12:51:00 +0800

    Feat: update search bar to use technology entries

--- techstackz/src/components/SearchBar/SearchBar.js
+++ techstackz/src/components/SearchBar/SearchBar.js
@@ -2,10 +2,27 @@ import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import "./SearchBar.css"
 import {Button} from "../Button/Button";
+import axios from "axios";
+import ReactDOM from 'react-dom';
+
+
+
+
+
 
 function SearchBar() {
   // states
   const [userInput, setUserInput] = useState("python")
+  const [arr, setArr] = useState([])
+
+  axios({
+    method: 'get', //you can set what request you want to be
+    url: 'http://3.26.190.60:8888/info/techList',
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(result=> { setArr(result["data"])
+  });
 
   // collect user input and store it to userInput state
   const handleTextChange = e => {
@@ -17,7 +34,15 @@ function SearchBar() {
       {/* search bar */}
       <div className={"search"}>
         <input className={"search__input"} defaultValue={"python"} type={"text"} id={"search"}
-               onChange={handleTextChange}/>
+               onChange={handleTextChange} list={"datalist"}/>
+      
+      <datalist id={"datalist"}>
+          {arr.map((value) =>{
+            return <option key={value} value={value} />
+          })}
+    </datalist>
+      
+
       </div>
       <p></p>
       <p></p>
@@ -28,5 +53,8 @@ function SearchBar() {
     </>
   )
 }
+
+
+
 
 export default SearchBar
